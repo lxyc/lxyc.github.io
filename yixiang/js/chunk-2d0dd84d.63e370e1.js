13200c53ce7ad8d44075cba7b9a7e383fce8bfa8
xiongyucheng <xiongyucheng@megvii.com>
2020-05-17 18:22:42 +0800

    add

--- yixiang/js/chunk-2d0dd84d.63e370e1.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0dd84d"],{8274:function(n,s,t){"use strict";t.r(s);var e=function(){var n=this,s=n.$createElement,t=n._self._c||s;return t("div",{staticClass:"industry-news"},[n._v("IndustryNews")])},d=[],u={},c=u,r=t("2877"),a=Object(r["a"])(c,e,d,!1,null,"6273b8d0",null);s["default"]=a.exports}}]);
